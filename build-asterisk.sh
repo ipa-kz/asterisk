72a8399da2865b2c1bd4da5fac54467b588b261c
Zhankin <zhankin@mail.ru>
2019-09-05 10:59:52 +0600

    Update build-asterisk.sh

--- build-asterisk.sh
+++ build-asterisk.sh
@@ -44,7 +44,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends --no-i
     xmlstarlet
 
 
-apt-get -y install wget nano vim sox iputils-ping python2.7 python-pip
+apt-get -y install wget nano vim sox iputils-ping python2.7 python-pip net-tools
 pip install smpplib
 #wget https://dev.mysql.com/get/Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.17-linux-ubuntu16.04-x86-64bit.tar.gz
 #gunzip mysql-connector-odbc-8.0.17-linux-ubuntu16.04-x86-64bit.tar.gz
